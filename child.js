b9977c11b7bdb5cc73c8900e1fad2e518a5177ee
Behzod <abdullozodabehzod@gmail.com>
2026-02-15 01:02:02 +0300

    New Education

--- child.js
+++ child.js
@@ -62,3 +62,17 @@ tabs.forEach(tab => {
     })
 })
 
+//InterScroll
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        }
+    })
+})
+
+document.querySelectorAll('.InterSection').forEach(el => {
+    observer.observe(el)
+})
+
